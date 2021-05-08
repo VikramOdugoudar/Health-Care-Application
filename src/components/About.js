5a05361a43589423ae89f6e18064fa8ea3148461
Vikramodugoudar <wittygamer32@gmail.com>
2021-05-08 09:41:54 +0530

    About Page Created

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="logo">
+        C<p>Curilux</p>
+        <p className="logo-p">User Application</p>
+      </div>
+      <footer className="footer">
+        <p>&copy; All Rights Reserved </p>
+        <p>Curilux India Pvt. Ltd. 2021</p>
+      </footer>
+    </div>
+  );
+};
+
+export default About;
